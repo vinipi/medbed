09978e1a679b759a2803e38b7eddaa41295eb72d
Raphael <raphael@corteks.fr>
2020-03-20 11:32:40 +0100

    fix react warning

--- front/src/routes/home.jsx
+++ front/src/routes/home.jsx
@@ -1,43 +1,19 @@
-import React, { Component } from "react";
-import { Collapse, Table, Tag, Popconfirm } from "antd";
-import { EditableTable } from "../components/editable";
+import React, { Component } from 'react';
+import { Collapse } from 'antd';
+import { EditableTable } from '../components/editable';
 
 const { Panel } = Collapse;
-const { Column, ColumnGroup } = Table;
-
-function callback(key) {
-    console.log(key);
-}
 
 const genExtra = () => {
     return <span>lits disponibles: 15/50</span>;
 };
-
-const data = [
-    {
-        key: "1",
-        label: "lits total",
-        value: "50",
-    },
-    {
-        key: "2",
-        label: "lits occupés",
-        value: "10",
-    },
-    {
-        key: "3",
-        label: "lits occupés COVID",
-        value: (<span>25</span>),
-    },
-];
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <Collapse
-                    defaultActiveKey={["0"]}
-                    onChange={callback}
+                    defaultActiveKey={['0']}
                 >
                     <Panel header="UF 1" key="1" extra={genExtra()}>
                         <div>
