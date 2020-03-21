73f1a6590da46acc9053a63e3c3922089a1707d3
Louis Frament <lf@novel-t.ch>
2020-03-21 23:23:02 +0100

    #19 init samu UI

--- suivichu/src/app/Features/samu/samu.component.ts
+++ suivichu/src/app/Features/samu/samu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Site } from 'src/app/Models/Site';
 
 @Component({
   selector: 'app-samu',
@@ -9,7 +10,23 @@ export class SamuComponent implements OnInit {
 
   constructor() { }
 
+  public cbOnlyWithAvailable = true;
+  public rdDisplayStyle: 'list' | 'map' = 'list';
+  public sitesResults: Site[] = undefined;
+
   ngOnInit(): void {
+    // TODO for debug only, call search api
+    this.sitesResults = [
+      {
+        id: 6,
+        name: "HOPITAL PEDIATRIE REEDUCATION BULLION",
+        bedOtherUsed: 11,
+        bedOtherAvailable: 22,
+        bedCovidUsed: 33,
+        bedCovidAvailable: 44,
+        ufs: []
+      }
+    ]
   }
 
 }
--- suivichu/src/app/Features/samu/samu.module.ts
+++ suivichu/src/app/Features/samu/samu.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { SamuRoutingModule } from './samu-routing.module';
 import { SamuComponent } from './samu.component';
+
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [SamuComponent],
   imports: [
+    FormsModule,
     CommonModule,
-    SamuRoutingModule
+    SamuRoutingModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatButtonModule,
   ]
 })
 export class SamuModule { }
